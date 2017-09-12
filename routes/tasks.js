a4c67d957a5a8f487383ddc5c9b2738c2092815d
cmtp <christian.tola@hotmail.com>
2017-09-11 22:57:12 -0400

    add middlewares and boot configuration

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,10 +1,9 @@
 module.exports = app => {
+    const Tasks = app.models.tasks;
+
     app.get('/tasks', (req, res) => {
-        res.json({
-            tasks: [
-                { title: 'Buy some shoes' },
-                { title: 'Fix nodebook' }
-            ]
-        });
+       Tasks.findAll({}, (tasks) => {
+           res.json({tasks: tasks});
+       });
     });
 };
