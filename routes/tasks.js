3b945895641f0fc3b31eafb1ad6467068e2de72c
cmtp <christian.tola@hotmail.com>
2017-09-15 21:22:17 -0400

    fix task route

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,5 +1,5 @@
 module.exports = app => {
-    const Tasks = app.db.models.tasks;
+    const Tasks = app.db.models.Tasks;
 
     app.get('/tasks', (req, res) => {
        Tasks.findAll({}).then(tasks => {
