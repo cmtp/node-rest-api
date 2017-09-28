14db5a9c7761a1741b7e1ed07f045d2c2cb8fed3
cmtp <christian.tola@hotmail.com>
2017-09-27 22:24:19 -0400

    add swagger and endpoint of login also

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 import express from 'express';
 import consign from 'consign';
+import argv from 'minimist';
+import swagger from 'swagger-node-express';
 
 const app = express();
+
+argv(process.argv.slice(2));
+const subpath = express();
+
+app.use('/v1', subpath);
+swagger.setAppHandler(subpath);
+app.use(express.static('dist'));
+swagger.setApiInfo({
+    title: "example API",
+    description: "API to do something, manage something...",
+    termsOfServiceUrl: "",
+    contact: "yourname@something.com",
+    license: "",
+    licenseUrl: ""
+});
+
+subpath.get('/v1', function (req, res) {
+    res.sendfile(__dirname + '/dist/index.html');
+});
+
+swagger.configureSwaggerPaths('', 'api-docs', '');
+
+var domain = 'localhost';
+if(argv.domain !== undefined)
+    domain = argv.domain;
+else
+    console.log('No --domain=xxx specified, taking default hostname "localhost".');
+var applicationUrl = 'http://' + domain;
+swagger.configure(applicationUrl, '1.0.0');
 
 consign({ verbose: false })
     .include('libs/config.js')
